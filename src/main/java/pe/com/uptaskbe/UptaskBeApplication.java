361cdb8eccd20d194010a478d4ad4708bb148ced
jmoreva21 <jackfrancomoreno15@gmail.com>
2025-11-01 22:45:02 -0500

    initialize project structure with Maven, add application configuration, and set up .gitignore and .gitattributes

---
+++ src/main/java/pe/com/uptaskbe/UptaskBeApplication.java
@@ -0,0 +1,11 @@
+package pe.com.uptaskbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UptaskBeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UptaskBeApplication.class, args);
+    }
+}
